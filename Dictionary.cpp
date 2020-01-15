7cbb3557b91bd98edc678e3fa620b69f7c253c5b
N0753746 <n0753746@my.ntu.ac.uk>
2020-01-15 23:01:48 +0000

    Initial commit

---
+++ Dictionary.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jpmon on 14/01/2020.
+//
+
+#include "Dictionary.h"
---
+++ Dictionary.h
@@ -0,0 +1,19 @@
+//
+// Created by jpmon on 14/01/2020.
+//
+
+#ifndef DICTIONARYCLASS_DICTIONARY_H
+#define DICTIONARYCLASS_DICTIONARY_H
+
+
+#include <string>
+
+class Dictionary {
+public:
+    using Key = std::string;
+    using Item = std::string;
+
+};
+
+
+#endif //DICTIONARYCLASS_DICTIONARY_H
